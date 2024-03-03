6a389195703b767f03a3c9f637f51262d5a6971a
Lutus163 <160641328+Lutus163@users.noreply.github.com>
2024-03-03 11:33:19 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = 0;
+
+let btn1 = document.getElementByld("btn1");
+let btn2 = document.getElementByld("btn2");
+let btn3 = document.getElementByld("btn3");
+let btn4 = document.getElementByld("btn4");
+let btn5 = document.getElementByld("btn5");
+
+btn1.addEventListener("click", function() {
+    item += 1000;
+    tg.MainButton.setText(String(item));
+    tg.MainButton.show();
+});
+
+btn2.addEventListener("click", function() {
+    item += 2000;
+    tg.MainButton.setText(String(item));
+    tg.MainButton.show();
+});
+
+btn3.addEventListener("click", function() {
+    item += 1200;
+    tg.MainButton.setText(String(item));
+    tg.MainButton.show();
+});
+
+btn4.addEventListener("click", function() {
+    item += 1400;
+    tg.MainButton.setText(String(item));
+    tg.MainButton.show();
+});
+
+btn5.addEventListener("click", function() {
+    item += 1250;
+    tg.MainButton.setText(String(item));
+    tg.MainButton.show();
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(item);
+    item = 0;
+});
+
+let usercard = document.getElementById("usercard");
+let p = document.createElement("p");
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+usercard.appendChild(p);
